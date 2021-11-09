d5af5cca939ee9df6a23e3d2eca02cc3462ee0a2
GeheimagentNr1 <geheimelache@hotmail.com>
2021-11-09 20:14:27 +0100

    Fixed crash with EndBlock

--- src/main/java/de/geheimagentnr1/manyideas_doors/elements/blocks/doors/special/end/DoorSpecialEndTile.java
+++ src/main/java/de/geheimagentnr1/manyideas_doors/elements/blocks/doors/special/end/DoorSpecialEndTile.java
@@ -1,15 +1,10 @@
 package de.geheimagentnr1.manyideas_doors.elements.blocks.doors.special.end;
 
-import de.geheimagentnr1.manyideas_core.elements.blocks.end_block.IEndBlock;
+import de.geheimagentnr1.manyideas_core.elements.blocks.end_block.EndBlockTile;
 import de.geheimagentnr1.manyideas_doors.elements.blocks.ModBlocks;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockRenderType;
-import net.minecraft.block.BlockState;
 import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.Direction;
-import net.minecraft.util.math.BlockPos;
 
 
 public class DoorSpecialEndTile extends TileEntity {
@@ -30,24 +25,8 @@ public class DoorSpecialEndTile extends TileEntity {
 	}
 	
 	//package-private
-	@SuppressWarnings( "deprecation" )
 	boolean shouldRender( Direction direction ) {
 		
-		if( world == null ) {
-			return true;
-		}
-		BlockPos direction_pos = pos.offset( direction );
-		BlockState direction_state = world.getBlockState( direction_pos );
-		if( direction_state.getRenderType() == BlockRenderType.INVISIBLE ) {
-			return true;
-		}
-		if( direction_state.getBlock() instanceof IEndBlock ) {
-			return direction_state.get( BlockStateProperties.OPEN );
-		}
-		if( direction_state.getBlock().getRenderLayer() == BlockRenderLayer.SOLID ) {
-			return !Block.hasSolidSide( direction_state, world, direction_pos, direction.getOpposite() );
-		} else {
-			return !direction_state.getBlock().isSolid( direction_state );
-		}
+		return EndBlockTile.shouldRender( world, pos, direction );
 	}
 }
