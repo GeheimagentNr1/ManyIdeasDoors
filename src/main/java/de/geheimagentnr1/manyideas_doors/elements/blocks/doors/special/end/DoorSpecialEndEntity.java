9f56e1f15ec0042bbccfe251e6b90cb6cfc23bac
GeheimagentNr1 <geheimelache@hotmail.com>
2021-11-09 22:13:13 +0100

    Fixed porting errors

--- src/main/java/de/geheimagentnr1/manyideas_doors/elements/blocks/doors/special/end/DoorSpecialEndEntity.java
+++ src/main/java/de/geheimagentnr1/manyideas_doors/elements/blocks/doors/special/end/DoorSpecialEndEntity.java
@@ -1,17 +1,12 @@
 package de.geheimagentnr1.manyideas_doors.elements.blocks.doors.special.end;
 
-import de.geheimagentnr1.manyideas_core.elements.blocks.end_block.EndBlockTile;
+import de.geheimagentnr1.manyideas_core.elements.blocks.end_block.EndBlockEntity;
 import de.geheimagentnr1.manyideas_doors.elements.blocks.ModBlocks;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.TheEndPortalBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
-import net.minecraft.state.properties.BlockStateProperties;
-import net.minecraft.tileentity.EndPortalTileEntity;
-import net.minecraft.util.Direction;
 
 import javax.annotation.Nonnull;
 
@@ -36,6 +31,6 @@ public class DoorSpecialEndEntity extends TheEndPortalBlockEntity {
 	@Override
 	public boolean shouldRenderFace( @Nonnull Direction face ) {
 		
-		return EndBlockTile.shouldRender( level, worldPosition, face );
+		return EndBlockEntity.shouldRender( level, worldPosition, face );
 	}
 }
